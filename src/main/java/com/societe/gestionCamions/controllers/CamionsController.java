6f12ba33e92b7a885301f1548b3d0d2e8be393ce
kabir othmane <othmanekabir1991@gmail.com>
2018-05-26 18:32:50 +0200

    first controller

---
+++ src/main/java/com/societe/gestionCamions/controllers/CamionsController.java
@@ -0,0 +1,12 @@
+package com.societe.gestionCamions.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CamionsController {
+	
+	
+
+}
